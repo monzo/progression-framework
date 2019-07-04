bc0e9d15b1ad43ffbda6b99040bb470cb0184825
Prav Patel <Prav.Patel@brighthr.com>
2019-07-04 17:38:16 +0100

    Added GA & Replaced Monzo's

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,8 @@
 const ReactGA = require('react-ga')
 
-ReactGA.initialize('UA-63104269-2')
+ReactGA.initialize('UA-143271060-1')
 ReactGA.set({
-  appName: 'Progression at Monzo'
+  appName: 'Progression at Bright'
 })
 
 exports.onRouteUpdate = (state) => {
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Monzo Progression',
+    title: 'Bright Progression',
   },
   plugins: [
     `gatsby-plugin-styled-components`,
