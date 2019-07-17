5fc21d3a2dcaf910599c1830c988333879d8d970
Paul2e <paultouhey@outlook.com>
2019-07-17 08:57:57 +0100

    Comment out local variables for devops running.

--- terraform/progression.tf
+++ terraform/progression.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
     version = "=1.28.0"
-    subscription_id = "${var.subscription_id}"
-    client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"
-    tenant_id       = "${var.tenant_id}"
+    #subscription_id = "${var.subscription_id}"
+    #client_id       = "${var.client_id}"
+    #client_secret   = "${var.client_secret}"
+    #tenant_id       = "${var.tenant_id}"
 }
 
 resource "azurerm_resource_group" "rg-progression-framework" {
