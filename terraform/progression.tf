afa1e5069d922366a02eab94ea228ce7666e1b60
Paul2e <paultouhey@outlook.com>
2019-07-17 10:29:47 +0100

    Enable parameters for provider. Bring in variables as env-vars.

--- terraform/progression.tf
+++ terraform/progression.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
     version = "=1.28.0"
-    #subscription_id = "${var.subscription_id}"
-    #client_id       = "${var.client_id}"
-    #client_secret   = "${var.client_secret}"
-    #tenant_id       = "${var.tenant_id}"
+    subscription_id = "${var.subscription_id}"
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
 }
 
 resource "azurerm_resource_group" "rg-progression-framework" {
@@ -23,7 +23,7 @@ resource "azurerm_storage_account" "progressionframework" {
 
 resource "null_resource" "azure-login-service-principle" {
   provisioner "local-exec" {
-    command = "az login --service-principal"
+    command = "az login --service-principal -u ${var.client_id} =p ${var.client_secret} --tenant ${var.tenant_id}"
   }
   depends_on = ["azurerm_storage_account.progressionframework"]
 }
