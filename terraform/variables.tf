cfd71d8fcbc3b19202b423cceb51d3268b628a1a
Paul2e <paultouhey@outlook.com>
2019-07-17 10:41:15 +0100

    Include blank variables definition. Override in vars.

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+    type = "string"
+    default = ""
+}
+
+variable "tenant_id" {
+    type = "string"
+    default = ""
+}
+
+variable "client_id" {
+    type = "string"
+    default = ""
+}
+
+variable "client_secret" {
+    type = "string"
+    default = ""
+}
